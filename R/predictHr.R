5df2a0210a578d229ea64e6e0ac374ebd2254a54
ashleyweir1 <weir.a@wehi.edu.au>
2023-10-31 16:36:50 +1100

    Updating predictHr function.

--- R/predictHr.R
+++ R/predictHr.R
@@ -1,10 +1,6 @@
 #' IdentifiHR
 #'
-#' The "predictHr" function utilises IdentifiHR, a pre-trained penalised
-#' logistic regression classifier, to predict homologous recombination status.
-#' While IdentifiHR was trained to classify high-grade serous ovarian
-#' carcinomas, it can be applied to other cancer types, though the accuracy of
-#' classification will be reduced.
+#' The "predictHr" function utilises IdentifiHR, a pre-trained penalised logistic regression classifier, to predict homologous recombination status. While IdentifiHR was trained to classify high-grade serous ovarian carcinomas, it can be applied to other cancer types, though the accuracy of classification will be reduced.
 #'
 #' @param y A matrix of gene expression counts, with samples presented in rows and genes presented in columns.
 #' @param scaled Logical. Has z-score scaling been performed? Default is TRUE, being that the counts matrix has been scaled. If FALSE, z-score scaling of each gene will be performed.
@@ -14,18 +10,45 @@
 #' @export
 #'
 #' @examples
+#' ## NOT RUN
+#' # data()
+#' # predictHr()
 #'
-predictHr <- function(y, lmHrSig) {
+
+predictHr <- function(y,
+                      scaled) {
+
+  if(scaled == "TRUE") {
+
+    print("Predicting HR status using scaled gene expression counts")
+
+    # Make predictions using the trained model
+    predictionHr <- predict(lmHrSig,
+                            newx = y,
+                            s = "lambda.min",
+                            type = c("class"))
+
+    return(predictionHr)
+  }
+
+  if(scaled == "FALSE") {
+
+    print("Z-score scaling gene expression counts and then predicting HR status using scaled gene expression counts")
+
+    # Z-score scale each row of the counts matrix
+    scaledCounts <- scale(y,
+                          center = TRUE,
+                          scale = TRUE)
+
+    # Make predictions using the trained model
+    predictionHr <- predict(lmHrSig,
+                            newx = scaledCounts,
+                            s = "lambda.min",
+                            type = c("class"))
+
+    return(predictionHr)
+  }
 
 }
+predictHr(testDataDe, scaled = TRUE)
 
-
-predict_with_model <- function(counts_matrix, trained_model) {
-  # Z-score scale each row of the counts matrix
-  scaled_counts <- scale(counts_matrix, center = TRUE, scale = TRUE)
-
-  # Make predictions using the trained model
-  predictions <- predict(trained_model, newdata = as.data.frame(scaled_counts))
-
-  return(predictions)
-}
