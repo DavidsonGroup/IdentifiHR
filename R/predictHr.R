96f142a312227731e30dc1317989cd87f44392da
ashleyweir1 <weir.a@wehi.edu.au>
2023-11-02 10:03:49 +1100

    Updating predictHr function to include logCPM transformation.

--- R/predictHr.R
+++ R/predictHr.R
@@ -3,7 +3,7 @@
 #' The "predictHr" function utilises IdentifiHR, a pre-trained penalised logistic regression classifier, to predict homologous recombination status. While IdentifiHR was trained to classify high-grade serous ovarian carcinomas, it can be applied to other cancer types, though the accuracy of classification will be reduced.
 #'
 #' @param y A numeric matrix of gene expression counts, with samples presented in rows and genes presented in columns.
-#' @param logCpm Logical. Have the raw gene expression counts been transformed into log counts per million? Default is TRUE, being that the counts matrix has been transformed. If FALSE, a log counts per million transformation will be applied.
+#' @param logCpm Logical. Have the raw gene expression counts been transformed into log2-counts-per-million? Default is TRUE, being that the counts matrix has been transformed. If FALSE, a log2-counts-per-million transformation will be applied.
 #' @param scaled Logical. Has z-score scaling been performed? Default is TRUE, being that the counts matrix has been scaled. If FALSE, z-score scaling of each gene will be performed.
 #' @return A data frame containing the sample identifier, the predicted HR status and the probability that this prediction is accurate.
 #' @export
@@ -14,6 +14,7 @@
 #' # library(dplyr)
 #' # library(tidyverse)
 #' # library(stats)
+#' # library(edgeR)
 #' # Load data
 #' # data(tcgaOvScaled)
 #' # Predict HR status with IdentifiHR
@@ -94,6 +95,8 @@ predictHr <- function(y,
     if(scaled == "TRUE") {
 
       print("Counts are being logCpm transformed")
+      
+      y <- cpm(y, log = TRUE)
 
       print("Predicting HR status using scaled gene expression counts")
 
@@ -126,6 +129,9 @@ predictHr <- function(y,
     if(scaled == "FALSE") {
 
       print("Counts are being logCpm transformed")
+      
+      y <- cpm(y, log = TRUE)
+      
       print("Z-score scaling gene expression counts and then predicting HR status using scaled gene expression counts")
 
       # Z-score scale each row of the counts matrix
@@ -168,4 +174,4 @@ predictHr <- function(y,
 }
 
 test <- predictHr(testDataDe, scaled = TRUE)
-
+library(edge)
