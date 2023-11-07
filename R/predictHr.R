a5de77f530a54e854f4d82a7daf27631df62181a
ashleyweir1 <weir.a@wehi.edu.au>
2023-11-07 16:06:24 +1100

    Separating preprocessing of counts from predict function.

--- R/predictHr.R
+++ R/predictHr.R
@@ -3,9 +3,10 @@
 #' The "predictHr" function utilises IdentifiHR, a pre-trained penalised logistic regression classifier, to predict homologous recombination status. While IdentifiHR was trained to classify high-grade serous ovarian carcinomas, it can be applied to other cancer types, though the accuracy of classification will be reduced.
 #'
 #' @param y A numeric matrix of gene expression counts, with samples presented in rows and genes presented in columns.
-#' @param logCpm Logical. Have the raw gene expression counts been transformed into log2-counts-per-million? Default is TRUE, being that the counts matrix has been transformed. If FALSE, a log2-counts-per-million transformation will be applied.
-#' @param scaled Logical. Has z-score scaling been performed? Default is TRUE, being that the counts matrix has been scaled. If FALSE, z-score scaling of each gene will be performed.
 #' @return A data frame containing the sample identifier, the predicted HR status and the probability that this prediction is accurate.
+#' @import dplyr
+#' @import tidyverse
+#' @importFrom stats predict
 #' @export
 #'
 #' @examples
@@ -20,17 +21,8 @@
 #' # Predict HR status with IdentifiHR
 #' # predictHr(tcgaOvScaled, scaled = TRUE)
 
-predictHr <- function(y,
-                      logCpm,
-                      scaled) {
-
-  if(logCpm == "TRUE") {
-
-    if(scaled == "TRUE") {
-
-      print("Counts have already been logCpm transformed")
+predictHr <- function(y) {
       print("Predicting HR status using scaled gene expression counts")
-
       # Make predictions using the trained model
       predictionHr <- stats::predict(lmHrSig,
                                      newx = y,
@@ -39,7 +31,6 @@ predictHr <- function(y,
         as.data.frame() %>%
         tibble::rownames_to_column(var = "Sample") %>%
         dplyr::rename(., hrPrediction = lambda.min)
-
       predictedProb <- stats::predict(lmHrSig,
                                       newx = y,
                                       s = "lambda.min",
@@ -47,97 +38,8 @@ predictHr <- function(y,
         as.data.frame() %>%
         tibble::rownames_to_column(var = "Sample") %>%
         dplyr::rename(., predictionProb = lambda.min)
-
       predictionHrDf <- left_join(predictionHr, predictedProb, by = "Sample")
-
       return(predictionHrDf)
     }
 
-  }
-
-  if(logCpm == "TRUE") {
-
-    if(scaled == "FALSE") {
-
-      print("Counts have already been logCpm transformed")
-      print("Z-score scaling gene expression counts and then predicting HR status using scaled gene expression counts")
-
-      # Z-score scale each row of the counts matrix
-      scaledCounts <- scale(y,
-                            center = TRUE,
-                            scale = TRUE)
-
-      # Make predictions using the trained model
-      predictionHr <- stats::predict(lmHrSig,
-                                     newx = y,
-                                     s = "lambda.min",
-                                     type = "class") %>%
-        as.data.frame() %>%
-        tibble::rownames_to_column(var = "Sample") %>%
-        dplyr::rename(., hrPrediction = lambda.min)
-
-      predictedProb <- stats::predict(lmHrSig,
-                                      newx = y,
-                                      s = "lambda.min",
-                                      type = "response") %>%
-        as.data.frame() %>%
-        tibble::rownames_to_column(var = "Sample") %>%
-        dplyr::rename(., predictionProb = lambda.min)
-
-      predictionHrDf <- left_join(predictionHr, predictedProb, by = "Sample")
-
-      return(predictionHrDf)
-    }
-
-  }
-
-  if(logCpm == "FALSE") {
-
-    if(scaled == "FALSE") {
-
-      print("Counts are being logCpm transformed")
-      
-      y <- cpm(y, log = TRUE)
-      
-      print("Z-score scaling gene expression counts and then predicting HR status using scaled gene expression counts")
-
-      # Z-score scale each row of the counts matrix
-      scaledCounts <- scale(y,
-                            center = TRUE,
-                            scale = TRUE)
-
-      # Make predictions using the trained model
-      predictionHr <- stats::predict(lmHrSig,
-                                     newx = y,
-                                     s = "lambda.min",
-                                     type = "class") %>%
-        as.data.frame() %>%
-        tibble::rownames_to_column(var = "Sample") %>%
-        dplyr::rename(., hrPrediction = lambda.min)
-
-      predictedProb <- stats::predict(lmHrSig,
-                                      newx = y,
-                                      s = "lambda.min",
-                                      type = "response") %>%
-        as.data.frame() %>%
-        tibble::rownames_to_column(var = "Sample") %>%
-        dplyr::rename(., predictionProb = lambda.min)
-
-      predictionHrDf <- left_join(predictionHr, predictedProb, by = "Sample")
-
-      return(predictionHrDf)
-    }
-
-  }
-
-  if(logCpm == "FALSE") {
-
-    if(scaled == "TRUE") {
-
-      print("Cannot transform data if it has already been scaled. Please use un-scaled counts data as an input.")
-    }
-
-  }
-}
-
-test <- predictHr(testDataDe, logCpm = FALSE, scaled = TRUE)
+test <- predictHr(t(test))
