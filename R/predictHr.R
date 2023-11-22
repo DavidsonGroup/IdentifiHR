404b15ab08bc8ed3a131b8d297cfb0639ec23092
ashleyweir1 <weir.a@wehi.edu.au>
2023-11-22 11:04:34 +1100

    Updating description in predictHR function.

--- R/predictHr.R
+++ R/predictHr.R
@@ -3,7 +3,7 @@
 #' The "predictHr" function utilises IdentifiHR, a pre-trained penalised logistic regression classifier, to predict homologous recombination status. While IdentifiHR was trained to classify high-grade serous ovarian carcinomas, it can be applied to other cancer types, though the accuracy of classification will be reduced.
 #'
 #' @param y A numeric matrix of gene expression counts, with samples presented in rows and genes presented in columns.
-#' @return A data frame containing the sample identifier, the predicted HR status and the probability that this prediction is accurate.
+#' @return A data frame containing the sample identifier, the predicted HR status and the probability that a given sample if HRP.
 #' @import dplyr
 #' @import tidyverse
 #' @importFrom stats predict
@@ -22,24 +22,24 @@
 #' # predictHr(tcgaOvScaled, scaled = TRUE)
 
 predictHr <- function(y) {
-      print("Predicting HR status using scaled gene expression counts")
-      # Make predictions using the trained model
-      predictionHr <- stats::predict(lmHrSig,
-                                     newx = y,
-                                     s = "lambda.min",
-                                     type = "class") %>%
-        as.data.frame() %>%
-        tibble::rownames_to_column(var = "Sample") %>%
-        dplyr::rename(., hrPrediction = lambda.min)
-      predictedProb <- stats::predict(lmHrSig,
-                                      newx = y,
-                                      s = "lambda.min",
-                                      type = "response") %>%
-        as.data.frame() %>%
-        tibble::rownames_to_column(var = "Sample") %>%
-        dplyr::rename(., predictionProb = lambda.min)
-      predictionHrDf <- left_join(predictionHr, predictedProb, by = "Sample")
-      return(predictionHrDf)
-    }
+  print("Predicting HR status using scaled gene expression counts")
+  # Make predictions using the trained model
+  predictionHr <- stats::predict(lmHrSig,
+                                 newx = y,
+                                 s = "lambda.min",
+                                 type = "class") %>%
+    as.data.frame() %>%
+    tibble::rownames_to_column(var = "Sample") %>%
+    dplyr::rename(., hrPrediction = lambda.min)
+  predictedProb <- stats::predict(lmHrSig,
+                                  newx = y,
+                                  s = "lambda.min",
+                                  type = "response") %>%
+    as.data.frame() %>%
+    tibble::rownames_to_column(var = "Sample") %>%
+    dplyr::rename(., predictionProb = lambda.min)
+  predictionHrDf <- left_join(predictionHr, predictedProb, by = "Sample")
+  return(predictionHrDf)
+}
 
 test <- predictHr(t(test))
