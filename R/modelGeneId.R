fe5385171f886136c4fc9c23b6b086adbbc3acf2
ashleyweir1 <weir.a@wehi.edu.au>
2024-08-23 10:38:14 +1000

    adding model gene id data script

---
+++ R/modelGeneId.R
@@ -0,0 +1,15 @@
+#' IdentifiHR
+#'
+#' Identifiers for model genes
+#'
+#' @format A data frame with 2604 rows and 3 columns:
+#' \describe{
+#'   \item{ENSEMBL}{A character vector of ensembl identifiers}
+#'   \item{HGNC}{A character vector of hgnc symbols}
+#'   \item{ENTREZ}{A numeric vector of entrez identifiers}
+#' }
+#' 
+#' @source Created for subsetting genes down to those required by identifiHR. Generated from top table of DE analysis.
+#' 
+#' @author Ashley L Weir, \email{weir.a@@wehi.edu.au}
+"modelGeneId"
