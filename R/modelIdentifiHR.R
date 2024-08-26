e960a33f783ac1fc1c53e4593a8574c3dde607ab
ashleyweir1 <weir.a@wehi.edu.au>
2024-08-26 11:09:50 +1000

    modify data format

--- R/modelIdentifiHR.R
+++ R/modelIdentifiHR.R
@@ -2,8 +2,10 @@
 #'
 #' Trained IdentifiHR logistic regression model that can predict HR status using the weighted expression of 209 genes.
 #'
+#' @docType data
 #' @format An object of class \code{"glmnet"}.
 #' @source Trained IdentifiHR model for predicting HR status.
+#' @keywords datasets
 #' 
 #' @author Ashley L Weir, \email{weir.a@@wehi.edu.au}
 "modelIdentifiHR"
