e6ccf4fc2dd663f50d2dfc660a6286a042ea8a25
ashleyweir1 <weir.a@wehi.edu.au>
2024-08-29 17:11:23 +1000

    add plotProb()

---
+++ R/plotProb.R
@@ -0,0 +1,33 @@
+#' Plot the probability that a sample is HRD, as predicted by IdentifiHR
+#'
+#' The "plotProb()" function allows you to visualise the probability of the input sample/s being "HRD", and to compare this against the probability distribution of the TCGA HGSC testing cohort analysed in our manuscript.
+#'
+#' @param y A data frame outputted by the "predictHr()" function, including columns "Sample", "hrPrediction" and "predictionProbability".
+#' @return A plot of the input cohort and the TCGA test cohort, against the probability of each sample being HRD.
+#' @export
+#'
+#' @author Ashley L Weir, \email{weir.a@@wehi.edu.au}
+#' @examples
+#' #See GitHub wiki
+
+plotProb <- function(y) {
+  
+  y$cohort <- "input"
+  joinCohort <- rbind(y, tcgaTestPred)
+  plot(x = factor(joinCohort$cohort),
+       y = joinCohort$predictionProbability, 
+       xlab = "Cohort",
+       ylab = "Probability of being HRD", 
+       col = "white")
+  points(x = factor(joinCohort$cohort),
+         y = joinCohort$predictionProbability, 
+         col = ifelse(joinCohort$hrPrediction == "HRD", "tomato", "darkturquoise"),
+         pch = 19,
+         cex = 0.8)
+  abline(h = 0.5, col = "black", lwd = 1, lty = 2)
+  legend("bottomleft", 
+         legend = c("HRD", "HRP"),
+         col = c( "tomato", "darkturquoise"),
+         pch = 20)
+  
+  }
