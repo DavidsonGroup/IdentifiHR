f2d8d2b8a6417c480c9d9ba98bed47731fccd326
ashleyweir1 <weir.a@wehi.edu.au>
2024-08-30 11:26:34 +1000

    remove weighted gene from example data

--- R/rawCounts.R
+++ R/rawCounts.R
@@ -1,9 +1,9 @@
 #' IdentifiHR: Example raw counts matrix for 10 samples
 #'
-#' Example raw counts matrix for 10 samples
+#' Example raw counts matrix for 10 samples, though note that the gene "ENSG00000107175" is missing from this data set and required for model predictions. This allows for the "interrogateMissingness()" and "plotMissingness()" functions to be explored.
 #'
 #' @docType data
-#' @format A gene expression counts table with 52125 rows, containing ensembl gene identifiers as rownames, and 10 columns, representing distinct samples.
+#' @format A gene expression counts table with 52124 rows, containing ensembl gene identifiers as rownames, and 10 columns, representing distinct samples.
 #' 
 #' @source Created as an example dataset for package testing and use.
 #' 
