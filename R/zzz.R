fd36b343498247ceff0db1487911ad617a59f1b6
ashleyweir1 <ashleyweir97@gmail.com>
2026-03-04 15:10:42 +1100

    Declare global variables for NSE

---
+++ R/zzz.R
@@ -0,0 +1,13 @@
+# zzz.R
+
+# Import 'is' function from methods
+#' @importFrom methods is
+NULL
+
+# Declare global variables for NSE
+utils::globalVariables(c(
+  "ensembl_id", "entrezgene_id", "hgnc_symbol",
+  "inputStatus", "modelGeneId", 
+  "modelMeanGenesIdentifiHR", "modelSDGenesIdentifiHR",
+  "normWeightedGene"
+))
